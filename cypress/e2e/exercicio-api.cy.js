6f12a96435829e081a06c9a59e57f06f89133a37
Gars <gabrielstraube@hotmail.com>
2025-08-08 16:30:14 -0300

    test(api): validação de contrato da listagem de usuários no exercicio-api.cy.js

--- cypress/e2e/exercicio-api.cy.js
+++ cypress/e2e/exercicio-api.cy.js
@@ -1,9 +1,32 @@
 /// <reference types="cypress" />
+const Joi = require('joi');
 
 describe('Testes da Funcionalidade Usuários', () => {
 
-  it('Deve validar contrato de usuários', () => {
-    //TODO: 
+  it.only('[GET] Deve validar contrato de usuários', () => {
+    const schema = Joi.object({
+      quantidade: Joi.number().required(),
+      usuarios: Joi.array().items(
+        Joi.object({
+          nome: Joi.string().required(),
+          email: Joi.string().email().required(),
+          password: Joi.string().optional(),
+          administrador: Joi.string().valid('true', 'false').required(),
+          _id: Joi.string().required()
+        })
+      ).required()
+    });
+
+    // Requisição e validação
+    cy.request('/usuarios').then((res) => {
+      expect(res.status).to.eq(200);
+      const valid = schema.validate(res.body);
+
+      // Se der erro de contrato, falha o teste mostrando o motivo
+      if (valid.error) {
+        throw new Error(valid.error);
+      }
+    });
   });
 
   it('Deve listar usuários cadastrados', () => {
